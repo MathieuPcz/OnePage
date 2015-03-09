14938dc24b91a23e923fee059af12fad7107b43f
MathieuPcz <mathieu.paczesny@gmail.com>
2015-03-09 21:57:41 +0100

    Add multi effect, not responsive functional

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
 			<nav>
 				<ul>
 					<li><a  class="home" href="#home">Accueil</a></li>
-					<li><a class="otherHome"  href="#about">Infos</a></li>
+					<li><a class="otherHome"  href="#about" id="infos">Infos</a></li>
 					<li><a class="otherHome"  href="#services">Services</a></li>
 					<li><a class="otherHome"  href="#skill">Compétences</a></li>
 					<li><a class="otherHome"  href="#project">Projets</a></li>
@@ -21,14 +21,26 @@
 				</ul>
 			</nav>
 		</div>
-		<div id="downScrow"><a href="#about"><img src="images/flechebas.png" alt="Clic ici"></a></div>
-		<div id="home">
+		<a href="#about"><div id="downScrow"><img src="images/flechebas.png" alt="Clic ici"></div></a>
+		<div id="zoomHome">
 			<div id="defilText">
-				<strong>Vous avez des projets ? des idées ?</strong>
-				<strong>Créatif de créer un site web à votre image</strong>
+				<ul>
+					<li>Vous avez des projets ? des idées ?</li>
+					<li>Je vous offre ma créativité et mes compétences</li>
+					<li>Pour un site web à votre image</li>
+				</ul>
 			</div>
+			<div id="home"></div>
 		</div>
-		<div id="about"></div>
+		<div id="about">
+			<div id="cercleProfil">
+				<div id="imgProfil">
+					<img src="images/profil.jpg" alt="Image de profil" id="profil">
+				</div>
+			</div>
+			<h2>A propos</h2>
+			<p>La créativité autorise chacun à commettre des erreurs. L'art c'est de savoir lesquelles garder.</p>
+		</div>
 		<div id="services"></div>
 		<div id="skill"></div>
 		<div id="project"></div>
@@ -39,6 +51,16 @@
 	<script type="text/javascript" src="js/jquery.parallax-1.1.3.js"></script>
 	<script type="text/javascript">
 		$(document).ready(function(){
+			/*defilement image profil*/
+			
+			function defilProfil(){
+				var largeurImage = $(window).width()*0.2;
+				 $("#profil").animate({width:largeurImage},5000).delay(2000)
+				 			 .animate({width:480},5000,defilProfil).delay(2000);
+				};
+
+				defilProfil();
+			
 			/*mise en place du paralax*/
 			$.localScroll();		
     		  $('#home').parallax("50%", 0.2); 
@@ -48,8 +70,10 @@
 
 			/*repete defilement haut/bas de la fleche*/ 
 			function bis(){
-				 $("#downScrow").animate({marginTop:450},'slow')
-			 				.animate({marginTop:500},'slow', bis);
+				var largeurImage = $(window).width()*0.36;
+				var largeurImageBas = ($(window).width()*0.36)+(largeurImage*0.05);
+				 $("#downScrow").animate({marginTop:largeurImage},'slow')
+			 				.animate({marginTop:largeurImageBas},'slow', bis);
 				};
 			bis();
 
@@ -71,8 +95,8 @@
 
 			var largeurEcran = $(window).width();
 			var zoomImage = largeurEcran + 100;
-			$('#home').animate({width:zoomImage},5000)
-					  .animate({width:largeurEcran},5000, repeatZoom);
+			$('#home').animate({width:zoomImage},5000).delay(1000)
+					  .animate({width:largeurEcran},5000, repeatZoom).delay(1000);
 			};
 			repeatZoom();
 			/*fixe color sur menu selectionné*/
@@ -82,9 +106,20 @@
 			    $(this).addClass("activeMenu");
 			});
 
+			/*defilement text*/
+			$('#defilText').fadeOut(0).fadeIn(2000);
+			$(function(){
+      setInterval(function(){
+         $("#defilText ul").animate({marginTop:-50},1500,function(){
+            $(this).css({marginTop:0}).find("li:last").after($(this).find("li:first"));
+         })
+      }, 4000);
+   });
+
 			/*clic fleche -> menu visible*/
 			$('#downScrow').click(function(){
 				$('#header').css('visibility','visible');
+				$('#infos').addClass("activeMenu");
 			})
 		});
 	</script>
