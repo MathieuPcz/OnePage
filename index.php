96299599f708a21609e6877e7f09d09e84754ff5
MathieuPcz <mathieu.paczesny@gmail.com>
2015-03-09 16:20:52 +0100

    Add jquery effect and Down-scrow

--- index.php
+++ index.php
@@ -12,17 +12,18 @@
 			</div>
 			<nav>
 				<ul>
-					<li><a  href="#home">Accueil</a></li>
-					<li><a  href="#about">Infos</a></li>
-					<li><a  href="#services">Services</a></li>
-					<li><a  href="#skill">Compétences</a></li>
-					<li><a  href="#project">Projets</a></li>
-					<li><a  href="#contact">Contact</a></li>
+					<li><a  class="home" href="#home">Accueil</a></li>
+					<li><a class="otherHome"  href="#about">Infos</a></li>
+					<li><a class="otherHome"  href="#services">Services</a></li>
+					<li><a class="otherHome"  href="#skill">Compétences</a></li>
+					<li><a class="otherHome"  href="#project">Projets</a></li>
+					<li><a class="otherHome"  href="#contact">Contact</a></li>
 				</ul>
 			</nav>
 		</div>
+		<div id="downScrow"><a href="#about"><img src="images/flechebas.png" alt="Clic ici"></a></div>
 		<div id="home"></div>
-		<div id="about"></div>
+		<div id="about">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam sapiente enim esse. Corporis saepe, vitae. Recusandae, quae beatae expedita sequi eos neque assumenda magnam vitae deleniti, sit sed ipsam a.</div>
 		<div id="services"></div>
 		<div id="skill"></div>
 		<div id="project"></div>
@@ -30,20 +31,46 @@
 	<script type="text/javascript" src="jquery.js"></script>
 	<script type="text/javascript">
 		$(document).ready(function(){
+			/*repete defilement haut/bas de la fleche*/ 
+			function bis(){
+				 $("#downScrow").animate({marginTop:450},'slow')
+			 				.animate({marginTop:500},'slow', bis);
+				};
+			bis();
+
+			/*lancement du menu une fois #home passé*/
+
+			$('.otherHome').click(function(){
+				$('#header').css('visibility','visible');
+			})
+				
+			$('.home').click(function(){
+				$('#header').css('visibility','hidden');
+			})
+				
+			
+
+
+			/*Zoom image de fond #home*/
+			function repeatZoom(){
+
 			var largeurEcran = $(window).width();
 			var zoomImage = largeurEcran + 100;
-			$('#home').animate({
-				width:zoomImage
-			},5000,function() {
-		   $('#home').animate({
-				width:largeurEcran
-		  },5000)});
+			$('#home').animate({width:zoomImage},5000)
+					  .animate({width:largeurEcran},5000, repeatZoom);
+			};
+			repeatZoom();
 			/*fixe color sur menu selectionné*/
-			$("a").click(function(e) {
+			$("li a").click(function(e) {
 			    e.preventDefault();
-			    $("a").removeClass("activeMenu");
+			    $("li a").removeClass("activeMenu");
 			    $(this).addClass("activeMenu");
 			});
+
+			/*clic fleche -> menu visible*/
+			$('#downScrow').click(function(){
+				$('#header').css('visibility','visible');
+			})
 		});
 	</script>
 	</body>
